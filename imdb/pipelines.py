b1651b44a47619571f377217eab86b8b09c956da
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-04-24 16:10:02 +0930

    Connection and upload to a MongoDB database

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import pymongo
 
+class MongodbPipeline(object):
+    collection_name = 'best_movies'
 
-class ImdbPipeline(object):
+    def open_spider(self,spider):
+        self.client = pymongo.MongoClient("mongodb+srv://marcelo:testtest@cluster0-jewlp.mongodb.net/test?retryWrites=true&w=majority")
+        self.db = self.client["IMDB"]
+    
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
