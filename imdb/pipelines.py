cc0eea534c40e14a493ab86360c7d00ac8c79555
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-04-24 17:52:45 +0930

    connection now to SQL lite

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -4,19 +4,44 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import logging
-import pymongo
 
-class MongodbPipeline(object):
-    collection_name = 'best_movies'
+import sqlite3
+
+class SQLlitePipeline(object):
 
     def open_spider(self,spider):
-        self.client = pymongo.MongoClient("mongodb+srv://marcelo:testtest@cluster0-jewlp.mongodb.net/test?retryWrites=true&w=majority")
-        self.db = self.client["IMDB"]
+        self.connection = sqlite3.connect("imdb.db")
+        self.c = self.connection.cursor()
+        try: 
+            self.c.execute('''
+                CREATE TABLE best_movies(
+                    title TEXT,
+                    year TEXT,
+                    duration TEXT,
+                    genre TEXT,
+                    rating TEXT,
+                    movie_url TEXT
+                )
+
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
     
     def close_spider(self, spider):
-        self.client.close()
+        self.connection.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert(item)
+        self.c.execute('''
+            INSERT INTO best_movies (title,year,duration,genre,rating,movie_url) VALUES(?,?,?,?,?,?)
+
+        ''', (
+            item.get('title'),
+            item.get('year'),
+            item.get('duration'),
+            item.get('genre'),
+            item.get('rating'),
+            item.get('movie_url')
+        ))
+        self.connection.commit()
         return item
